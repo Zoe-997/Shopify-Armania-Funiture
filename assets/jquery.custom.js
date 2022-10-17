25d0aee5007486a3dfb5c036625201ed774e34e4
ZoeTrinh <thuminh10011997@gmail.com>
2022-10-17 08:34:35 +0700

    upload Shopify theme

---
+++ assets/jquery.custom.js
@@ -0,0 +1,7 @@
+var Alothemes = window.Alothemes || {};
+Alothemes.custom =  Alothemes.custom || {};
+
+Alothemes.custom.SocialShare = function(){
+  var getHeight = $('.get-height').height();
+  $('.main-menu li.level-0>a').css('line-height', getHeight + 'px'); 
+}();
